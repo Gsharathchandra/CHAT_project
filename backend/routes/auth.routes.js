530ee848c17b3014a51ea792a2dcdb21c98a47ba
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-03 11:58:17 +0530

    save

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,20 +1,10 @@
 import express from "express"
+import { login, logout, signup } from "../controllers/auth.controlller"
 const router  = express.Router()
 
 
-router.get('/signup',(req,res)=>{
-    res.send("signup")
-    
-})
-
-router.get('/login',(req,res)=>{
-    res.send("signup")
-    
-})
-
-router.get('/logout',(req,res)=>{
-   res.send("signup")
-    
-})
+router.get('/signup',signup)
+router.get('/login',login)
+router.get('/logout',logout)
 
 export default router;
