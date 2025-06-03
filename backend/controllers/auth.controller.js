368b77208a2f20799c965fb3131a35ea1a1bfce4
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-03 11:59:24 +0530

    save

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 export const signup = (req,res)=>{
-
+res.send("working")
 }
 export const login = (req,res)=>{
-    
+res.send("working")
 }
 export const logout = (req,res)=>{
-    
+res.send("working")
 }
