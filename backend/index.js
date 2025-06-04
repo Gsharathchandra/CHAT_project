f69f33ab21dfdc6fc40325dca0cc397080276a2c
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-04 12:12:28 +0530

    SAVE

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
 import authRoutes from "./routes/auth.routes.js"
+const PORT = process.env.PORT
 const app = express()
 
 app.use('/api/auth',authRoutes)
-app.listen(5001,()=>{
-    console.log("app is running");
+app.listen(PORT,()=>{
+    console.log("app is running ON "+PORT);
     
 })
