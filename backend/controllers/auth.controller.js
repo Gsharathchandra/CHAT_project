38490adb32ba604b1ead0025f8b36423a1e343cb
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-04 21:15:45 +0530

    save all

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,40 @@
-export const signup = (req,res)=>{
-res.send("working")
-}
-export const login = (req,res)=>{
-res.send("working")
-}
-export const logout = (req,res)=>{
-res.send("working")
-}
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    //using bcryptjs to hash the password
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password should atleast be 6 charecters" });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "email already exists" });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+    if (newUser) {
+        
+    } else {
+      res.status(400).json({ message: "invalid user data" });
+    }
+  } catch (error) {}
+};
+
+
+
+export const login = (req, res) => {
+  res.send("working");
+};
+
+
+export const logout = (req, res) => {
+  res.send("working");
+};
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.routes.js"
 import { connectDB } from "./lib/db.js"
 const PORT = process.env.PORT
 const app = express()
+app.use(express.json())
 
 app.use('/api/auth',authRoutes)
 app.listen(PORT,()=>{
