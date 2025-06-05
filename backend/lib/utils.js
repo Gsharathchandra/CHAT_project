a48953c67e8d68da2146bf90357010c97bc79ab5
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-05 20:22:59 +0530

    save

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,11 +1,22 @@
 import jwt from "jsonwebtoken"
-export const generateToken = (userID,res)=>{
- const token = jwt.sign({userID},process.env.JWT_SECRET,{expiresIn : "7d"})
- res.cookie("jwt",token,{
-    maxAge : 7*24*60*60*1000,
-    httpOnly : yes,
+// export const generateToken = (userID,res)=>{
+//  const token = jwt.sign({userID},process.env.JWT_SECRET,{expiresIn : "7d"})
+//  res.cookie("jwt",token,{
+//     maxAge : 7*24*60*60*1000,
+//     httpOnly : yes,
+//     sameSite: "strict",
+//     secure:process.env.NODE_ENV !== "development",
+//  })
+//  return token;
+// }
+
+export const generateToken = (userID, res) => {
+  const token = jwt.sign({userID}, process.env.JWT_SECRET, {expiresIn: "7d"})
+  res.cookie("jwt", token, {
+    maxAge: 7*24*60*60*1000,
+    httpOnly: true,  // Fixed from 'yes' to 'true'
     sameSite: "strict",
-    secure:process.env.NODE_ENV !== "development",
- })
- return token;
+    secure: process.env.NODE_ENV !== "development",
+  })
+  return token
 }
