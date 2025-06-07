2c20971d0dc496517cd59dee3452c97a1ee697d9
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-07 11:34:29 +0530

    save

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 dotenv.config()
 import authRoutes from "./routes/auth.routes.js"
 import { connectDB } from "./lib/db.js"
 const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth',authRoutes)
 app.listen(PORT,()=>{
