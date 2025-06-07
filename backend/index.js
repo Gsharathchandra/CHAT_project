a32cb1fd757978a8f200f70f4bcb19bd116fbb10
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-07 20:29:42 +0530

    moving to frontend

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,15 @@ import cookieParser from "cookie-parser"
 dotenv.config()
 import authRoutes from "./routes/auth.routes.js"
 import { connectDB } from "./lib/db.js"
+import messageRoutes from "./models/message.model.js"
 const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth',authRoutes)
+app.use('/api/auth',messageRoutes)
+
 app.listen(PORT,()=>{
     console.log("app is running ON :"+PORT);
     connectDB();
